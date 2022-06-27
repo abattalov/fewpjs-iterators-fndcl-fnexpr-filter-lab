2298a22cfe3f55d64858ce59ae1a609033178688
abattalov <abattalov14@gmail.com>
2022-06-26 21:25:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(source, sought) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase() === sought.toLowerCase()
+    )
+  }
+
+
+function fuzzyMatch(source, testString){
+    return source.filter(possibleMatch => possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0 )
+
+}
+
+function matchName(source, nameSearch){
+    return source.filter(name => name.name === nameSearch)
+
+}
